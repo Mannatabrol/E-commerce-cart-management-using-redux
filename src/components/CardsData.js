3216c2ce2654225d449196f4512bf10f09762513
Mannat Abrol <mannatabrol28@gmail.com>
2024-03-28 07:48:18 +0530

    Final commit

--- src/components/CardsData.js
+++ src/components/CardsData.js
@@ -6,7 +6,7 @@ const Cardsdata = [
         address: "North Indian, Biryani, Mughlai",
         delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
         somedata: " 1175 + order placed from here recently",
-        price: 350,
+        price: 550,
         rating: "3.8",
         arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
         qnty:0
@@ -18,7 +18,7 @@ const Cardsdata = [
         address: "Street Food",
         delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
         somedata: " 2525 + order placed from here recently",
-        price: 25,
+        price: 20,
         rating: "3.9",
         arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
          qnty:0
@@ -54,7 +54,7 @@ const Cardsdata = [
         address: "North Indian",
         delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
         somedata: "1050 + order placed from here recently",
-        price: 210,
+        price: 610,
         rating: "4.0",
         arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
          qnty:0
